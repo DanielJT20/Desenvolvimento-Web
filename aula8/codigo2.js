e9828a41ffc91efe74f7eeec750842c796ec592a
Daniel Teixeira <127428783+DanielJT20@users.noreply.github.com>
2024-11-08 22:29:19 -0300

    att

--- aula8/codigo2.js
+++ aula8/codigo2.js
@@ -21,12 +21,31 @@ const montaPagina = (dados) =>{
     const descri = document.createElement("p");
     descri.innerHTML = dados.detalhes;
     body.appendChild(descri);
-
- 
-
-};
-pega_json(`https://botafogo-atletas.mange.li/2024-1/${id}`).then( (r) => montaPagina(r)
-)
-
+    const posi = document.createElement("p");
+    posi.innerHTML = dados.posicao;
+    body.appendChild(posi);
+}
+ if(sessionStorage.getItem('logado')){
 
 
+pega_json(`https://botafogo-atletas.mange.li/2024-1/${id}`).then( (r) => montaPagina(r));
+ }else{
+    document.body.innerHTML = "<h1>você não tem a senha</h1>"
+}
+
+
+const achaCookie = (chave) =>{
+    const lista = document.cookie.split("; ");
+    const par = lista.find(
+        ( e ) => e.startsWith(`${chave}=`)
+    )
+
+    return par.split("=")[1]
+
+}
+console.log('altura', achaCookie('altura'));
+
+const dadosSessionStorage = sessionStorage.getItem('dados');
+const obj = JSON.parse(dadosSessionStorage)
+
+console.log('número de jogos: ', obj.nJogos);
